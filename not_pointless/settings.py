2e9591516890f5df42233718498b17ccac8ec317
Fred Trotter <fred.trotter@gmail.com>
2025-05-25 17:50:09 -0400

    lets statically have the static files

--- not_pointless/settings.py
+++ not_pointless/settings.py
@@ -84,7 +84,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'not_pointless',
+    'not_pointless.apps.NotPointlessConfig',
 ]
 
 MIDDLEWARE = [
